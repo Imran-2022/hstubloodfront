af1670e6d75f41defc70b12565984a792097f645
Imran-2022 <mdimranulhaque.2022@gmail.com>
2022-05-05 03:40:03 +0600

    pagination add

--- src/Main.js
+++ src/Main.js
@@ -7,6 +7,7 @@ import Login from './components/Login/Login';
 import Register from './components/Login/Register';
 import Navigation from './components/Navigation/Navigation';
 import NoMatch from './components/NoMatch/NoMatch';
+
 const Main = () => {
     return (
         <>
@@ -14,7 +15,7 @@ const Main = () => {
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="*" element={<NoMatch />} />
-            <Route path="/donors" element={<Donors />} />
+            <Route path="/donors" element={<Donors/>} />
             <Route path="/donate" element={<Donate />} />
             <Route path="/log-in" element={<Login />} />
             <Route path="/register" element={<Register />} />
--- src/components/Donors/Donors.js
+++ src/components/Donors/Donors.js
@@ -1,10 +1,50 @@
-import React from 'react';
+import React, { useState } from "react";
+import JsonData from "./MOCK_DATA.json";
+import ReactPaginate from "react-paginate";
 import "./Donors.css"
 const Donors = () => {
+    const [users, setUsers] = useState(JsonData.slice(0, 200));
+    const [pageNumber, setPageNumber] = useState(0);
+
+    const usersPerPage = 10;
+    const pagesVisited = pageNumber * usersPerPage;
+
+    const displayUsers = users
+        .slice(pagesVisited, pagesVisited + usersPerPage)
+        .map((user) => {
+            return (
+                <div className="user">
+                    <h3>{user.firstName}</h3>
+                    <h3>{user.lastName}</h3>
+                    <h3>{user.email}</h3>
+                </div>
+            );
+        });
+
+    const pageCount = Math.ceil(users.length / usersPerPage);
+
+    const changePage = ({ selected }) => {
+        setPageNumber(selected);
+    };
+
+
     return (
-        <div>
-            <p>BLOOD GROUPS</p>
-        </div>
+        <>
+            <div className="App">
+                {displayUsers}
+                <ReactPaginate
+                    previousLabel={"Previous"}
+                    nextLabel={"Next"}
+                    pageCount={pageCount}
+                    onPageChange={changePage}
+                    containerClassName={"paginationBttns"}
+                    previousLinkClassName={"previousBttn"}
+                    nextLinkClassName={"nextBttn"}
+                    disabledClassName={"paginationDisabled"}
+                    activeClassName={"paginationActive"}
+                />
+            </div>
+        </>
     );
 };
 
