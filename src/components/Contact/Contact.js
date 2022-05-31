46d4d015feb722f41fa9afccbc2035260ae852ab
Imran-2022 <mdimranulhaque.2022@gmail.com>
2022-05-31 19:24:09 +0600

    contact page responsive

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -3,17 +3,15 @@ import "./Contact.css"
 import {BiCurrentLocation } from "react-icons/bi";
 const Contact = () => {
     return (
-        <div>
+        <div >
             <div className="contact-header d-flex justify-content-center align-items-center">
                 <h2>Contact Us</h2>
             </div>
             <section className="location w-75 m-auto py-5 my-5">
                 <iframe className="w-100" title="our location in google map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3595.1734626403168!2d88.65398201497212!3d25.698680183665378!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39e4ad0b5d200fa7%3A0x2e0c8f8bb049efa2!2sHajee%20Mohammad%20Danesh%20Science%20%26%20Technology%20University!5e0!3m2!1sen!2sbd!4v1653392894239!5m2!1sen!2sbd" width="600" height="450" style={{ border: "0" }} loading="lazy"></iframe>
             </section>
-
-            
-            <section className="w-75 m-auto">
-                <div className="mt-5 d-flex justify-content-between">
+            <section className="w-75 m-auto ">
+                <div className="mt-5 d-flex justify-content-between contactUSS">
                     <div className="contact-col">
                         <div className="d-flex align-items-center mb-5">
                             <BiCurrentLocation className="fab " />
@@ -48,7 +46,6 @@ const Contact = () => {
                     </div>
                 </div>
             </section>
-
         </div>
     );
 };
